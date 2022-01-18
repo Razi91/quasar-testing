99a4bd2bdf8ff4b5d647e3b5a7a30c244006a49e
jkonieczny <konieczny.jakub91@gmail.com>
2022-01-18 08:29:22 +0100

    direct component

---
+++ src/components/TranslationEntry2.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from '@jest/globals';
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+
+import TranslationEntry from './TranslationEntry2.vue'
+import { mount, shallowMount } from '@vue/test-utils';
+
+installQuasarPlugin();
+
+describe('TranslationEntry2', () => {
+    it('mount', () => {
+        const wrapper = mount(TranslationEntry, {
+        });
+        expect(wrapper).toBeTruthy();
+    })
+    it('shallowMount', () => {
+        const wrapper = shallowMount(TranslationEntry, {
+        });
+        expect(wrapper).toBeTruthy();
+    })
+})
---
+++ src/components/TranslationEntry2.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-item>
+    <q-item-section aside>
+      <QBtn icon='arrow_left'/>
+    </q-item-section>
+  </q-item>
+</template>
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+import { debounce, QBtn, QItem, QItemSection } from 'quasar';
+import {useVModel} from '@vueuse/core'
+
+const entry = () => ({
+    langs: {
+        pl: 'Wpis',
+        en: 'Entry',
+    },
+});
+
+export default defineComponent({
+    name: 'TranslationEntry',
+    components: {
+        QBtn,
+        QItem,
+        QItemSection,
+      },
+      props:{
+          modelValue: {
+              type: Object as PropType<any>,
+              default: () => entry()
+          }
+      },
+      setup (props, {emit}) {
+          const entry = useVModel(props, 'modelValue', emit);
+          return {
+              entry
+          }
+      },
+});
+</script>
